9f5c0b34c1d4e29043625eee412be216f8613d20
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-20 14:45:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,10 +20,11 @@ def apply_coupons(cart, coupons)
     coupon_name = item_name + " W/COUPON"
     cart[coupon_name] = Hash.new
     cart[coupon_name][:price] = coupon[:cost]
+    cart[coupon_name][:num] = 0
     while cart[item_name][:count] >= coupon[:num]
       cart[item_name][:count] -= coupon[:num]
+      cart[coupon_name][:count] += 1
     end
-    cart[coupon_name][:count] = coupon[:num]
   end
   cart
   
