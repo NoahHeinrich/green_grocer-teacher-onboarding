ebdfcc4da829396adce383d4d0e77a36b58d3fed
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-20 14:35:33 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,24 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = Hash.new
+  cart.each do |item|
+    item_name = item.keys[0]
+    if new_cart.has_key?(item_name) 
+      new_cart[item_name][:count] += 1 
+    else
+      new_cart[item_name] = item[item_name]
+      new_cart[item_name][:count] = 1 
+    end
+  end
+  new_cart
 end
 
+
+
 def apply_coupons(cart, coupons)
-  # code here
+  item_name = coupons[0][:item]
+  coupon_name = item_name + " W/COUPON"
+  cart[coupon_name][:price] = coupons[item_name][:cost]
+  cart[coupon_name][:clearance] = cart[item_name][:clearance]
 end
 
 def apply_clearance(cart)
